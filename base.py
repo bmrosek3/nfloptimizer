6babf87a540318edfb5dd59acb0b1253e80d3256
Ben Mrosek <ben_mrosek@aol.com>
2022-12-31 16:53:40 -0500

    week 17

--- base.py
+++ base.py
@@ -10,7 +10,7 @@ def whole():
     import pulp
     
     # variables declared for week and year number for reuse throughout code
-    week_num = 16
+    week_num = 17
     year_num = 22
     
     #################################### Setup ####################################
